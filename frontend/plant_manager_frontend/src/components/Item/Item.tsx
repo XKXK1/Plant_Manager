e257974661d5c6e800265ace5c41c1c784c236f8
XKXK1 <deryauyargil@hotmail.de>
2023-04-15 18:13:48 +0200

    add onlick effect

--- frontend/plant_manager_frontend/src/components/Item/Item.tsx
+++ frontend/plant_manager_frontend/src/components/Item/Item.tsx
@@ -1,34 +1,41 @@
 import CardData from "./IData";
 import { ItemStyle } from "./ItemStyle";
+import { useState } from "react";
+
 
 interface Props {
   cardData: CardData;
 }
 
 const Item = ({ cardData }: Props) => {
+  const [selected, setSelected] = useState(false);
+
   return (
-    <ItemStyle>
-
-    <div className="card">
-      <img
-        className="card-img-top"
-        src={cardData.imageUrl}
-        alt="Card image cap"  
-      />
-      <div className="card-body">
-        <h5 className="card-title">{cardData.title}</h5>
-        <p className="card-text">{cardData.description}</p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-        <a href="#" className="btn btn-secondary">
-          Go somewhere
-        </a>
+    <ItemStyle variant={selected}>
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={cardData.imageUrl}
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{cardData.title}</h5>
+          <p className="card-text">{cardData.description}</p>
+          <a
+            href="#"
+            className="btn btn-primary"
+            onClick={() => {
+              setSelected(!selected);
+            }}
+          >
+            {selected===true ? 'LOL thanks' : 'pls press'}
+          </a>
+          <a href="#" className="btn btn-secondary">
+            Go somewhere
+          </a>
+        </div>
       </div>
-      
-    </div>
     </ItemStyle>
-
   );
 };
 
--- frontend/plant_manager_frontend/src/components/Item/ItemStyle.tsx
+++ frontend/plant_manager_frontend/src/components/Item/ItemStyle.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
-export const ItemStyle = styled.div`
+interface ButtonProps {
+    variant: boolean;
+  }
+
+export const ItemStyle = styled.div<ButtonProps>`
   margin: 10px 10px 10px 10px;
+  opacity: ${props => props.variant ? 1.0 : 0.5};
   &:hover {
     transform: scale(1.02);
     opacity: 1.0;
